54e3ed303cf5d5285c12b3fc52afa1ef038632bd
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-02 22:55:53 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,9 +1,23 @@
-const prompt = require('prompt');
-prompt.start();
+let arr = [1,6,2,6,3];
+let ededOrta;
+let sum = 0;
+let count = 0;
+let index; 
+let sum2 = 0;
+for(let i = 0;i<arr.length;i++){
+    // console.log(arr[i]);
+    sum = sum + arr[i];
+    index = i+1;
+}
+console.log(sum);
 
+ededOrta = (sum / index);
+console.log(ededOrta);
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+for(let  i = 0;i<arr.length;i++){
+    if(arr[i]>ededOrta){
+        count++;
+        sum2 = sum2 + arr[i];
+    }
+}
+console.log(sum2,count);
