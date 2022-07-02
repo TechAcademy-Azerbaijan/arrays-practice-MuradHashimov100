ba1508324fb5ad19318df2a93a9e098d5f24edf4
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-02 23:03:01 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,9 +1,9 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let arr = [9,5,-7,-7,5,-9,-4];
+let count = 0;
+for (let i = 1; i < arr.length; i++) {
+    if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+        count++;
+        console.log(arr[i]);
+    }
+}
+console.log(count);
